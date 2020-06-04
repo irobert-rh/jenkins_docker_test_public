c3c7bc6bbde06ca6104a3907cefe8dc5ca45cdd0
robert.luo <59629912+irobert-rh@users.noreply.github.com>
2020-06-04 15:39:42 -0700

    Update buildKubeImage.groovy

--- vars/buildKubeImage.groovy
+++ vars/buildKubeImage.groovy
@@ -39,22 +39,5 @@ def call(app, build_airflow = false, build_job_name = "image-builder", dockerfil
                 }
             }
         }
-        post {
-            always {
-                script {
-                    switch (currentBuild.currentResult) {
-                        case "FAILURE":
-                            sh "echo \"kubernetes_image_build_result 1\" | curl --data-binary @- \"http://pushgateway.service.robinhood:9091/metrics/job/kubernetes_image_build_result/role/\${APP}\""
-                            break
-                        case "SUCCESS":
-                            sh "echo \"kubernetes_image_build_result 0\" | curl --data-binary @- \"http://pushgateway.service.robinhood:9091/metrics/job/kubernetes_image_build_result/role/\${APP}\""
-                            break
-                        case "UNSTABLE":
-                            sh "echo \"kubernetes_image_build_result 2\" | curl --data-binary @- \"http://pushgateway.service.robinhood:9091/metrics/job/kubernetes_image_build_result/role/\${APP}\""
-                            break
-                    }
-                }
-            }
-        }
     }
 }
