d20e6d882dfa24f2f4166d0127c612b150459f91
robert.luo <59629912+irobert-rh@users.noreply.github.com>
2020-06-04 14:43:23 -0700

    Update buildKubeImage.groovy

--- vars/buildKubeImage.groovy
+++ vars/buildKubeImage.groovy
@@ -9,14 +9,13 @@ def call(app, build_airflow = false, build_job_name = "image-builder", dockerfil
             ADD_LATEST_TAG = "${add_latest_tag}"
         }
         stages {
-            stage("Build") {
-                
-                aqua locationType: 'local', localImage: 'nodejscn/node', caCertificates: false, customFlags: '', hideBase: false, hostedImage: '', notCompliesCmd: '', onDisallowed: 'ignore', policies: '', register: false, registry: '', showNegligible: false
-                            
+            stage("Build") {   
                 when {
                     expression { build_job_name == currentBuild.projectName }
                 }
                 steps {
+                    aqua locationType: 'local', localImage: 'nodejscn/node', caCertificates: false, customFlags: '', hideBase: false, hostedImage: '', notCompliesCmd: '', onDisallowed: 'ignore', policies: '', register: false, registry: '', showNegligible: false
+                 
                     catchError {
                         withCredentials(bindings: [sshUserPrivateKey(credentialsId: 'github-key', keyFileVariable: 'JENKINS_SSH_PRIVATE')]) {
                             checkout scm
